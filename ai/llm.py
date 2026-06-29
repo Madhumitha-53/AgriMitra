d1629651c149e9f047aa95663a4f64d6526dacfe
Madhumitha-53 <madhumithaelango783@gmail.com>
2026-06-29 12:15:36 +0530

    Refactor API key retrieval to use environment variables

--- ai/llm.py
+++ ai/llm.py
@@ -1,5 +1,4 @@
 import os
-import streamlit as st
 
 from langchain_openai import ChatOpenAI
 from langchain_groq import ChatGroq
@@ -8,26 +7,26 @@ from langchain_nvidia_ai_endpoints import ChatNVIDIA
 from ai.prompt import prompt
 
 
-def get_secret(key):
-    return st.secrets.get(key, os.getenv(key))
-
 
 nvidia = ChatNVIDIA(
-    api_key=get_secret("NVIDIA_API_KEY"),
+    api_key=os.getenv("NVIDIA_API_KEY"),
     model="meta/llama-3.1-8b-instruct"
 )
 
 
+
 openai = ChatOpenAI(
-    api_key=get_secret("OPENAI_API_KEY"),
+    api_key=os.getenv("OPENAI_API_KEY"),
     model="gpt-4o-mini"
 )
 
 
+
 groq = ChatGroq(
-    api_key=get_secret("GROQ_API_KEY"),
+    api_key=os.getenv("GROQ_API_KEY"),
     model="llama-3.1-8b-instant"
 )
+
 
 
 llm = nvidia.with_fallbacks(
@@ -38,14 +37,17 @@ llm = nvidia.with_fallbacks(
 )
 
 
+
 def get_response(question):
 
     chain = prompt | llm
 
+
     response = chain.invoke(
         {
-            "question": question
+            "question":question
         }
     )
+
 
     return response.content
